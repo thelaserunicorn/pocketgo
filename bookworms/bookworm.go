8087cf0b56ad95ac008d4868eb32707277489e59
Rashid <rashideram2@gmail.com>
2025-05-30 22:24:52 +0600

    added test

---
+++ bookworms/bookworm.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"encoding/json"
+	"os"
+)
+
+type Book struct {
+  Author string `json:"author"`
+  Title string `json:"title"` 
+}
+
+type Bookworm struct {
+  Name string `json:"name"`
+  Books []Book `json:"books"`
+}
+
+
+func loadBookworms(filePath string)([]Bookworm, error){
+  f, err := os.Open(filePath)
+  if err != nil {
+    return nil, err
+  }
+  defer f.Close()
+  var bookworms []Bookworm
+  err = json.NewDecoder(f).Decode(&bookworms)
+  if err != nil {
+    return nil, err
+  }
+  return bookworms, nil
+}
---
+++ bookworms/go.mod
@@ -0,0 +1,3 @@
+module pocketgo/bookworms
+
+go 1.24.3
